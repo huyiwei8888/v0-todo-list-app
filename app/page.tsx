c71f1beae0db464cbdaed85f4b18cfce3f57624c
huyiwei8888 <huyiwei@fuzhi.ai>
2025-11-20 11:07:20 +0800

    Update page.tsx
    feat: age=18

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ const DEFAULT_CATEGORIES = ["今天的任务", "学习任务", "工作任务"]
 const name = 'huyiwei-local';
 const desc = 'this is local v0 test3 可以编辑';
 const title = 'happy birthday - remote github test333';
+const age = 18;
 
 export default function Home() {
   const [categories, setCategories] = useState<string[]>(DEFAULT_CATEGORIES)
